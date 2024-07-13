b45f90c845fefd99caece49038656d35c7dae501
Vinnie Moth <vinniemoth@gmail.com>
2024-07-13 20:32:11 -0300

    Adicionada decoração de header + animação

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -7,23 +7,27 @@ import "./header.css";
 
 // Importações Imagens
 import logo from "../../images/logo.webp";
+import tape from "../../images/tape.png";
 
 export default function Header() {
   return (
-    <div className="header">
-      <div>
-        <Link to="/">
-          <img id="logo" src={logo} alt="" />
-        </Link>
-      </div>
-      <div>
-        <input type="text" placeholder="Pesquisar" />
-      </div>
-      <div className="botoes">
-        <div className="icon">
-          <FeatherIcon icon="menu" />
+    <div className="head_wrapper">
+      <div className="header">
+        <div>
+          <Link to="/">
+            <img id="logo" src={logo} alt="" />
+          </Link>
+        </div>
+        <div>
+          <input type="text" placeholder="Pesquisar" />
+        </div>
+        <div className="botoes">
+          <div className="icon">
+            <FeatherIcon icon="menu" />
+          </div>
         </div>
       </div>
+      <div className="head_tape"> </div>
     </div>
   );
 }
