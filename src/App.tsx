0ea61e02700a80f0d5a1bf12350eff2721abdf99
VINICIUS AUGUSTO DE ARAUJO <vinicius.aaraujo9@senacsp.edu.br>
2024-07-15 19:32:58 -0300

    Adicionada nova página 'Browse', com componentes.

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./style/App.css";
 import Home from "./pages/home";
 import { Routes, Route } from "react-router-dom";
+import Browse from "./pages/browse";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/browse" element={<Browse />} />
       </Routes>
     </div>
   );
---
+++ src/pages/browse.tsx
@@ -0,0 +1,12 @@
+import Header from "../components/header/header";
+import Footer from "../components/footer";
+
+export default function Browse() {
+  return (
+    <div>
+      <Header></Header>
+      <h1>Browse</h1>
+      <Footer></Footer>
+    </div>
+  );
+}
