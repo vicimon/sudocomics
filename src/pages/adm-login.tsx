febf20dd864c8c3cff1092d0bcc0149975a10730
VINICIUS AUGUSTO DE ARAUJO <vinicius.aaraujo9@senacsp.edu.br>
2024-08-23 21:39:05 -0300

    Adição e alteração da rota de Login

--- src/pages/adm-login.tsx
+++ src/pages/adm-login.tsx
@@ -4,16 +4,17 @@ import "../style/App.css";
 
 export default function AdmLogin() {
   return (
-    <div className="conteiner-login-fundo">
+    <div>
       <Header></Header>
+      <div id="wrapper-login">
       <div className="conteiner-login">
         <h1 className="tittle-login">ADM Login</h1>
         <p>Email</p>
         <input className="text-bar-login"  type="Email" placeholder="Email/Usuario"/>
         <p>Senha</p>
         <input className="text-bar-login"  type="Password" placeholder="Senha"/>
-        <p>Teste</p>
-        <input className="text-bar-login"  type="text" placeholder="bucetinha"/>
+        <button id="login-button" type="submit"> Login </button>
+      </div>
       </div>
       <Footer></Footer>
     </div>
