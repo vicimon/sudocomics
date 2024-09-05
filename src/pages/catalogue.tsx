b6edbfa1ca4d9315938a9b30486a5462aa791788
Vinicius <vicimon15@gmail.com>
2024-09-04 22:25:19 -0300

    mais alterações no catlog

--- src/pages/catalogue.tsx
+++ src/pages/catalogue.tsx
@@ -2,17 +2,22 @@ import Header from "../components/header/header";
 import Footer from "../components/footer";
 import "../style/catlogue_style.css";
 import CatlogCollection from "../components/catlog_colec";
+import seta from "../images/seta.png"
 
 export default function Catalogue() {
   return (
     <div>
       <Header />
       <div>
-        <div className="catlog_page">
-          <div className="catlog_content">
-            <CatlogCollection />
+          <div className="catlog_page">
+            <div className="catlog_content">
+              <CatlogCollection />
+          <div className="botao_cat">
+            <button className="but_1"><img src={seta} width={100}/></button>
+            <button className="but_2"><img src={seta} width={100}/></button>
           </div>
-        </div>
+            </div>
+          </div>
       </div>
       <Footer />
     </div>
